fd171650aa017636d76ba7744853491d9098120c
Dmytro <msmsivoisv@gmail.com>
2025-10-06 21:29:49 +0300

    Add some new buttons

---
+++ Test_project/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Test_project.Models;
+using Test_project.ViewModels;
+
+namespace Test_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CurrencyViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewModel = new CurrencyViewModel();
+            CurrenciesGrid.ItemsSource = viewModel.Currencies;
+            DataContext = viewModel;
+
+
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Add_Currency();
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (Currency)CurrenciesGrid.SelectedItem;
+            viewModel.Delete_Currency(selected);
+        }
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (Currency)CurrenciesGrid.SelectedItem;
+            viewModel.Edit_Currency(selected);
+        }
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Refresh_Currency();
+        }
+    }
+}
---
+++ Test_project/ViewModels/CurrencyViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Test_project.Models;
+
+namespace Test_project.ViewModels
+{
+    public class CurrencyViewModel
+    {
+        public ObservableCollection<Currency> Currencies { get; set; }
+
+        public CurrencyViewModel()
+        {
+            Currencies = new ObservableCollection<Currency>()
+            {
+                new Currency("US dollar", "USD", 1.0m),
+                new Currency("Euro", "EUR", 0.92m),
+                new Currency("Japanese Yen", "JPY", 144.5m)
+            };
+        }
+        public void Add_Currency()
+        {
+            Currencies.Add(new Currency("New Currency", "New", 0m));
+        }
+
+        public void Delete_Currency(Currency currency)
+        {
+            if (currency != null)
+                Currencies.Remove(currency);
+        }
+
+        public void Edit_Currency(Currency currency)
+        {
+            if (currency != null)
+                currency.Price += 1;
+        }
+
+        public void Refresh_Currency()
+        {
+            Currencies.Clear();
+            Currencies.Add(new Currency("US dollar", "USD", 1.0m));
+            Currencies.Add(new Currency("Euro", "EUR", 0.92m));
+            Currencies.Add(new Currency("Japanese Yen", "JPY", 144.5m));
+        }
+    }
+}
